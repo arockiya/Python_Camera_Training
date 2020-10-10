2c44b9b8eec4e5eebf9e8ac5a99de1d1fc101d57
Arockiya Selva <arockiyaselva@gmail.com>
2020-10-10 11:18:20 +0400

    Read and show an image

--- YT_H1_Chapter1.py
+++ YT_H1_Chapter1.py
@@ -1,2 +1,10 @@
 import cv2
 print("Pakage Imported")
+
+img = cv2.imread('Resources/Lenna.png')
+
+cv2.imshow("Output",img)
+
+# Waitkey 0 - infinite time
+# waitkey 1- 1 millisecond, 1000 - 1 second
+cv2.waitKey(0)
